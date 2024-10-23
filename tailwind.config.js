9562544ac0e085611699e5e4d6360fe50f4f0893
AkshayaHaridas <akshayaharidas001@gmail.com>
2024-10-23 11:37:02 +0530

    Added input and search button and few styling on the page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/WeatherForecast.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        url: 'url("https://img.freepik.com/premium-photo/vast-green-meadow-with-fluffy-white-clouds-bright-blue-sky_38013-57515.jpg?w=1060")',
+      },
+      gridTemplateRows: {
+        // Simple 16 row grid
+        10: "repeat(16,1fr)",
+      },
+      gridTemplateColumns: {
+        // Simple 16 row grid
+        1: "100vw",
+      },
+    },
   },
   plugins: [],
-}
-
+};
